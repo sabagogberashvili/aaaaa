511e89e44df71e0e4e71fb6a4e6f665b893a6d7a
saba gogberashvili <sgogberashvili@gmail.com>
2024-10-27 00:03:28 +0400

    Initial commit

---
+++ myproject/articles/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    views = models.IntegerField(default=0)
+    is_published = models.BooleanField(default=True)
+    create_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
